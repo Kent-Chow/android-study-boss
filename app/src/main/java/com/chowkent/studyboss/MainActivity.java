b5acb6875c2dee346ffba1c1956da3aded313c7a
Kent Chow <kent.chow@hotmail.ca>
2016-08-25 01:48:44 -0400

    Implemented basic timer functions

--- app/src/main/java/com/chowkent/studyboss/MainActivity.java
+++ app/src/main/java/com/chowkent/studyboss/MainActivity.java
@@ -1,13 +1,99 @@
 package com.chowkent.studyboss;
 
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button startButton;
+    private Button pauseButton;
+    private Button resetButton;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+    private TextView timerValue;
+
+    private long startTime = 0L;
+
+    private Handler customHandler = new Handler();
+
+    long timeInMilliseconds = 0L;
+    long timeSwapBuff = 0L;
+    long updatedTime = 0L;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timerValue = (TextView) findViewById(R.id.timer);
+
+        startButton = (Button) findViewById(R.id.start_button);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                startTime = SystemClock.uptimeMillis();
+                customHandler.postDelayed(updateTimerThread, 0);
+                pauseButton.setEnabled(true);
+                startButton.setEnabled(false);
+
+            }
+        });
+
+        pauseButton = (Button) findViewById(R.id.pause_button);
+        pauseButton.setEnabled(false);
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                timeSwapBuff += timeInMilliseconds;
+                customHandler.removeCallbacks(updateTimerThread);
+                pauseButton.setEnabled(false);
+                startButton.setEnabled(true);
+
+            }
+        });
+
+        resetButton = (Button) findViewById(R.id.reset_button);
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                startTime = 0L;
+                timeInMilliseconds = 0L;
+                timeSwapBuff = 0L;
+                updatedTime = 0L;
+                timerValue.setText(getResources().getString(R.string.timer_string));
+                customHandler.removeCallbacks(updateTimerThread);
+                pauseButton.setEnabled(false);
+                startButton.setEnabled(true);
+
+            }
+        });
+    }
+
+    private Runnable updateTimerThread = new Runnable() {
+
+        public void run() {
+
+            timeInMilliseconds = SystemClock.uptimeMillis() - startTime;
+
+            updatedTime = timeSwapBuff + timeInMilliseconds;
+
+            int secs = (int) (updatedTime / 1000);
+            int mins = secs / 60;
+            int hours = mins / 60;
+            secs = secs % 60;
+            int seconds100 = (int) ((updatedTime / 10) % 100);
+            timerValue.setText(String.format("%02d", hours) + ":"
+                    + String.format("%02d", mins) + ":"
+                    + String.format("%02d", secs) + ":"
+                    + String.format("%02d", seconds100));
+            customHandler.postDelayed(this, 0);
+        }
+    };
 }
+
